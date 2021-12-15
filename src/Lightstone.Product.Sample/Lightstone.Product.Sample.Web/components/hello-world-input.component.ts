c082374e4bfcedb12e69214cb0276c3e666b81e1
Jacques Snyman <jacquess@lightstone.co.za>
2021-12-15 15:49:31 +0200

    Added MFE
    AB#73980

---
+++ src/Lightstone.Product.Sample/Lightstone.Product.Sample.Web/components/hello-world-input.component.ts
@@ -0,0 +1,7 @@
+import { LitElement } from "lit";
+import { customElement } from 'lit/decorators.js';
+
+@customElement('hello-world-input')
+export class HelloWorldInput extends LitElement {
+
+}
---
+++ src/Lightstone.Product.Sample/Lightstone.Product.Sample.Web/main.ts
@@ -0,0 +1,2 @@
+import { HelloWorldInput } from "./components/hello-world-input.component";
+import { HelloWorld } from "./components/hello-world.component";
